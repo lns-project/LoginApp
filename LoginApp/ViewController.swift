a857f6cdaf9eeadbc0812b6d25718d03993b8162
Ginza31 <lns-project@yandex.ru>
2023-01-07 18:51:00 +0300

    Setup project

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -9,11 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var userPasswordTF: UITextField!
+    
+    let currentUserName = "User"
+    let currentPassword = "Password"
+
+    @IBAction func forgotNamePressed(_ sender: UIButton!) {
+        showAlert(title: "Oops!", message: "Your name is \(currentUserName) 😉")
+    }
+    
+    @IBAction func forgotPasswordPressed(_ sender: UIButton) {
+        showAlert(title: "Oops!", message: "Your password is \(currentPassword) 😉")
+    }
+    
+    @IBAction func logInPressed(_ sender: UIButton) {
+        guard userNameTF.text == currentUserName, userPasswordTF.text == currentPassword else {
+            showAlert(title: "Invalid login or password",
+                      message: "Please, enter correct login and password")
+            userPasswordTF.text = ""
+            return
+        }
+        performSegue(withIdentifier: "secondVCSegue", sender: sender)
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let loginVC = segue.destination as? WelcomeViewController, let text = userNameTF.text {
+            loginVC.userName = text
+        }
+    }
+    
+    @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
+        guard segue.source is WelcomeViewController else { return }
+        userNameTF.text = ""
+        userPasswordTF.text = ""
+    }
+    
+    // Метод для скрытия клавиатуры тапом по экрану
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+    }
 }
 
+
+// MARK: - UIAlertController
+extension ViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by Динара Шарафутдинова on 07.01.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet var greetingsIcon: UITextField!
+    @IBOutlet var greetingUserTF: UITextField!
+    
+    var userName: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setGradientBackground()
+        greetingUserTF.text = "Welcome, \(userName ?? "")!"
+        greetingsIcon.text = "👋"
+    }
+    
+    private func setGradientBackground() {
+        let colorTop =  UIColor(red: 192.0/255.0, green: 129.0/255.0, blue: 143.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 136.0/255.0, green: 154.0/255.0, blue: 219.0/255.0, alpha: 1.0).cgColor
+                    
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+                
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+}
